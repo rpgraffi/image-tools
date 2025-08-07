57bc96d7d730de25af60b85f990094c06f769ac4
raffi.git <51893700+rpgraffi@users.noreply.github.com>
2025-08-07 23:50:47 +0200

    Initial Commit

---
+++ image-tools/image_toolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_toolsApp.swift
+//  image-tools
+//
+//  Created by Raphael Wennmacher on 07.08.25.
+//
+
+import SwiftUI
+
+@main
+struct image_toolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ image-toolsTests/image_toolsTests.swift
@@ -0,0 +1,16 @@
+//
+//  image_toolsTests.swift
+//  image-toolsTests
+//
+//  Created by Raphael Wennmacher on 07.08.25.
+//
+
+import Testing
+
+struct image_toolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
