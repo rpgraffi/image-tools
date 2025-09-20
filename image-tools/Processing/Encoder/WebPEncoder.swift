8cb3cd2c5af529f88e7541a8ce51d6f378950b76
raffi.git <51893700+rpgraffi@users.noreply.github.com>
2025-09-21 01:08:55 +0900

    (feat): update WebP encoding method: Adjust the encoding method parameter from 0 to 0.5 in WebPEncoder to improve image compression quality during export.

--- image-tools/Processing/Encoder/WebPEncoder.swift
+++ image-tools/Processing/Encoder/WebPEncoder.swift
@@ -13,7 +13,7 @@ struct WebPEncoder: CustomImageEncoder {
         let nsImage = NSImage(cgImage: cgImage, size: NSSize(width: pixelSize.width, height: pixelSize.height))
         var options: [SDImageCoderOption: Any] = [:]
         if let q = compressionQuality { options[.encodeCompressionQuality] = q }
-        options[.encodeWebPMethod] = 0
+        options[.encodeWebPMethod] = 0.5
         guard let data = SDImageWebPCoder.shared.encodedData(with: nsImage, format: .webP, options: options) else {
             throw ImageOperationError.exportFailed
         }
